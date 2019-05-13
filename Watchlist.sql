189937b28cce7ac4a6dd3132f47cd74a848fc363
Vidya <vidya@amiti.in>
2019-05-13 15:47:05 +0530

    testing

--- Watchlist.sql
+++ Watchlist.sql
@@ -1,2 +1,2 @@
-Select * From Purchase_Receipts Where Purchase_Date = Sysdate-3
+Select * From Purchase_Receipts Where Purchase_Date between sysdate-5 and sysdate 
 and vehicle_id in (select vehicle_id from vehicles where car_group_config_id = 153 and active=1);
